42306806c55ec24dbe756117aa26ca5913646a42
Tristan <tschartman41@gmail.com>
2022-01-13 09:51:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# beta β
 __version = "A-0.2"
 print(f"build version {__version}\n\nsubmit feedback at https://github.com/tsj845/console-rpg \nto get syntax highlighting for .amly files follow the steps on https://github.com/tsj845/amlytheme \n")
 
